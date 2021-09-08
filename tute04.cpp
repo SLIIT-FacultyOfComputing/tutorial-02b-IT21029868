3478521ea7637a94f224388f8ca25501a62655fc
Anuththara Divyanjalie <87685510+IT21029868@users.noreply.github.com>
2021-09-08 02:10:47 +0000

    Completed

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,24 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+  int i;
+  long fact = 1;
+
+  for(i= no; i>=1;i--)
+  {
+    fact = fact * i;
+  }
+  return fact;
+}
+
+long nCr(int n,int r)
+{
+  long nCr;
+
+  nCr = Factorial(n) / (Factorial (r)* Factorial(n-1));
+
+  return nCr;
+}
